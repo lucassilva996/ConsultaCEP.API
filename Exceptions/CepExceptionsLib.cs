4673f5c595afbe78b66828ec471c61efced41c7f
lucassilva996 <lucassilva996@outlook.com>
2022-11-11 01:14:15 -0300

    criada camada de serviço e tratativa de exceptions

---
+++ Exceptions/CepExceptionsLib.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsultaCEP.API.Exceptions
+{
+    public class CepExceptionsLib : Exception
+    {
+        public CepExceptionsLib()
+        {
+        }
+
+        public CepExceptionsLib(string message)
+            : base(message)
+        {
+        }
+
+        public CepExceptionsLib(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Service/CepServices.cs
@@ -0,0 +1,30 @@
+﻿using ConsultaCEP.API.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsultaCEP.API.Service
+{
+    public class CepServices
+    {
+        static readonly HttpClient client = new HttpClient();
+        public static async Task<string> GetEnderecoViaCEP(int cep, string type)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync($"https://viacep.com.br/ws/{cep}/{type}/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+    
+                return responseBody;
+            }
+            catch(Exception ex)
+            {
+                throw new CepExceptionsLib(ex.Message);
+            }
+        }
+    }
+}
